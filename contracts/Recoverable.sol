20d3de043846a93de212638ef5d155cfc3b6a67d
Filipe Gonçalves <filipe@impossible.com>
2018-05-25 16:33:35 +0100

    Visibility fixes. Address type coercion on comparison.

--- contracts/Recoverable.sol
+++ contracts/Recoverable.sol
@@ -17,14 +17,14 @@ contract Recoverable is Ownable {
 
   /// @dev This will be invoked by the owner, when owner wants to rescue tokens
   /// @param token Token which will we rescue to the owner from the contract
-  function recoverTokens(ERC20Basic token) onlyOwner public {
+  function recoverTokens(ERC20Basic token) onlyOwner internal {
     token.transfer(owner, tokensToBeReturned(token));
   }
 
   /// @dev Interface function, can be overwritten by the superclass
   /// @param token Token which balance we will check and return
   /// @return The amount of tokens (in smallest denominator) the contract owns
-  function tokensToBeReturned(ERC20Basic token) public returns (uint) {
-    return token.balanceOf(this);
+  function tokensToBeReturned(ERC20Basic token) internal returns (uint) {
+    return token.balanceOf(address(this));
   }
 }
--- contracts/UpgradeableToken.sol
+++ contracts/UpgradeableToken.sol
@@ -121,7 +121,7 @@ contract UpgradeableToken is StandardTokenExt {
    * This allows us to set a new owner for the upgrade mechanism.
    */
   function setUpgradeMaster(address master) public {
-      if (master == 0x0) revert();
+      if (master == address(0)) revert();
       if (msg.sender != upgradeMaster) revert();
       upgradeMaster = master;
   }
